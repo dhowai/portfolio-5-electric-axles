3513de34389ccf7851f44bf17446c980ea2a4fda
dhowai <dhowai.dev@gmail.com>
2022-04-06 20:46:35 +0000

    Add sizes to product model and size selctor to product details template

--- products/models.py
+++ products/models.py
@@ -31,6 +31,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     in_stock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
