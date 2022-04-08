553ea624e7e7a9941bc98fd93d03ee5be6d20f80
dhowai <dhowai.dev@gmail.com>
2022-04-08 20:14:10 +0000

    Add toasts

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 
 def view_basket(request):
@@ -10,6 +12,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of the specific product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -30,6 +33,7 @@ def add_to_basket(request, item_id):
             basket[item_id] += quantity
         else:
             basket[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your basket Successfully')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
