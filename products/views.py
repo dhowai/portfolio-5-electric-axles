a4455d24a28b9cdcb587863f3455d3647bf1ad40
dhowai <dhowai.dev@gmail.com>
2022-04-22 16:13:19 +0000

    Finish add product functionality and fix products with no image logic

--- products/views.py
+++ products/views.py
@@ -53,7 +53,17 @@ def category_list(request, category_slug):
 
 def add_product(request):
     """Add a product to the store"""
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product due to invalid form.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
