b753e1a264a4fbedc2eb8b0b876c8098f2566ed1
dhowai <dhowai.dev@gmail.com>
2022-04-30 15:12:16 +0000

    Remove secret key and set debug

--- electric_axles/settings.py
+++ electric_axles/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-skvszcc^n0%8k8xb9k67&iebjbhlr&2me-5^=-d*xy!_@)9h__'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['portfolio-5-electric-axles.herokuapp.com', 'localhost']
 
